1d323846401bb04f81efa0daa85632ce5ed2f711
Niclas Lund <niclas.lund@telia.com>
2022-02-11 12:55:56 +0100

    Added Character class and examples

---
+++ src/resources.py
@@ -0,0 +1,22 @@
+# imports
+# global variables
+# classes
+class Character:
+    
+    def __init__(self, name, health, damage, armor):
+        self.name = name
+        self.health = health
+        self.damage = damage
+        self.armor = armor
+    
+    def __str__(self):
+        return f'Name: {self.name}\nHealth: {self.health}\nDamage: {self.damage}\nArmor: {self.armor}'
+
+# functions
+def hello():
+    print("Hello World")
+# main code
+
+
+
+
