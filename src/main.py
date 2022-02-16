3d8d8e01e0237a5b9b1e9273accdc95d36c5da15
Niclas Lund <niclas.lund@telia.com>
2022-02-16 11:17:54 +0100

    fixed traceback bug in new_fight

--- src/main.py
+++ src/main.py
@@ -36,6 +36,7 @@ def new_fight(players: list, enemies: list):
         else:
             print(f"{target.get_name()} was attacked by {char.get_name()}.")
             print(f"{target.get_name()} has {target.get_health()} healthpoints left.")
+        if len(enemies) == 0 or len(players) == 0: break
     
     
 
